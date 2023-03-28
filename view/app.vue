6953bbea4f0e064961a2957a13f94a15677c3060
jiawing <183164869@qq.com>
2023-03-29 00:00:55 +0800

    vue环境支持(vue/tsx/jsx)+vite+ts+less

---
+++ view/app.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>支持不同的写法</div>
+    <exp2></exp2>
+    <exp1></exp1>
+    <exp></exp>
+</template>
+<script lang="ts">
+import exp2 from "./components/exp2";
+import exp1 from "./components/exp1.vue";
+import exp from "./components/exp";
+import { defineComponent, ref } from "vue";
+export default defineComponent({
+    components: { exp2, exp1, exp },
+});
+</script>
---
+++ view/components/exp1.vue
@@ -0,0 +1,9 @@
+<script lang="tsx">
+import { defineComponent } from "vue";
+export default defineComponent({
+    name: "app",
+    setup(props, ctx) {
+        return () => <div>这是一个.vue支持tsx的写法</div>;
+    },
+});
+</script>
