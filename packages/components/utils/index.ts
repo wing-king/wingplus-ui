7ea7084ab04ae10ef86677432b2852c079f4928b
jiawing <183164869@qq.com>
2023-04-15 23:37:38 +0800

    :sparkles: feat(custom): 添加通用函数

--- packages/components/utils/index.ts
+++ packages/components/utils/index.ts
@@ -4,3 +4,6 @@ export * from "./create";
 export * from "./props";
 export * from "./validate";
 export * from "./format";
+export * from "./basic";
+export * from "./interceptor";
+export * from "./dom";
