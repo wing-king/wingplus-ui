ca546ca5ae2c31d1bb4996c042499f50f713676d
jiawing <183164869@qq.com>
2023-04-15 16:52:26 +0800

    :sparkles: feat(custom): 添加通用函数

---
+++ packages/components/utils/basic.ts
@@ -0,0 +1,3 @@
+export const inBrowser = typeof window !== "undefined";
+
+export type Numeric = number | string;
--- packages/components/utils/index.ts
+++ packages/components/utils/index.ts
@@ -2,3 +2,5 @@ export * from "./with-install";
 export * from "./config";
 export * from "./create";
 export * from "./props";
+export * from "./validate";
+export * from "./format";
