0ec0da42a614bc392a7ec497b24590d4f5629354
jiawing <183164869@qq.com>
2023-04-21 01:14:32 +0800

    :sparkles: feat(custom): 添加use相关方法

--- packages/wingplus-use/index.ts
+++ packages/wingplus-use/index.ts
@@ -3,3 +3,6 @@ export * from "./useRect";
 export * from "./onMountedOrActivated";
 export * from "./useEventListener";
 export * from "./utils";
+export * from "./useRelation";
+export * from "./CustomFieldInjectionValue";
+export * from "./useChildren";
